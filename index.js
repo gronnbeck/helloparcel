b3940dfa3c910c2084c21f4b24be9c742614dc86
Ken Grønnbeck <kengroenn@gmail.com>
2018-05-19 16:42:05 +0200

    And with Elm

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import another component
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import HelloJavaScript from './main';
 import {PrintHello} from './tslib'
 import {HelloTypeScript} from './tscomp'
@@ -10,7 +11,12 @@ PrintHello("world")
 ReactDOM.render(
   <div>
     <HelloJavaScript name="world" />
-    <HelloTypeScript name="world"/>
+    <HelloTypeScript name="world" />
   </div>,
   document.getElementById('app')
 );
+
+import('./main.elm').then(Elm => {
+    var mountNode = document.getElementById('elmMain');
+    var app = Elm.Main.embed(mountNode);
+});
